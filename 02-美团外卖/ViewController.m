64b33d31b19bb5d1e7187102875b70f88388649a
Zed <linyanzuo1222@gmail.com>
2016-08-01 14:28:22 +0800

    LaunchScreen 方式显示启动图片, 添加放大动画

--- 02-美团外卖/ViewController.m
+++ 02-美团外卖/ViewController.m
@@ -14,14 +14,36 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self showLaunchImage];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+/**
+ *  LaunchScreen中配置启动图片的布局设置时, 约束为 left | right | bottom | height (与父视图相等) 
+ *  否则会有布局错误的提示!
+ */
+- (void)showLaunchImage
+{
+    // --------  将 LaunchScreen.storyboard中的视图获取出来, 并且放大显示 --------
+    // 获取Storyboard文件
+    UIStoryboard *launchScreen = [UIStoryboard storyboardWithName:@"LaunchScreen" bundle:nil];
+    // 加载入口的视图控制器
+    UIViewController *initVC = [launchScreen instantiateInitialViewController];
+    // 获取根视图
+    UIView *rootView = initVC.view;
+    
+    [self.view addSubview:rootView];
+    
+    // 放大的动画效果
+    [UIView animateWithDuration:2.0 animations:^{
+        rootView.transform = CGAffineTransformMakeScale(3.0, 3.0);
+        rootView.alpha = 0;
+    } completion:^(BOOL finished) {
+        [rootView removeFromSuperview];
+    }];
 }
 
 @end
