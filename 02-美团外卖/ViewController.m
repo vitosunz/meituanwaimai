ebe6228852e552b2bfd7e1a4eac5e7b9a3d8e093
Zed <linyanzuo1222@gmail.com>
2016-08-01 15:25:20 +0800

    添加PCH文件并测试

--- 02-美团外卖/ViewController.m
+++ 02-美团外卖/ViewController.m
@@ -21,6 +21,11 @@
     [self showLaunchImage];
 }
 
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    [SVProgressHUD showSuccessWithStatus:@"Hello world"];
+}
+
 /**
  *  LaunchScreen中配置启动图片的布局设置时, 约束为 left | right | bottom | height (与父视图相等) 
  *  否则会有布局错误的提示!
@@ -30,19 +35,19 @@
     // --------  将 LaunchScreen.storyboard中的视图获取出来, 并且放大显示 --------
     // 获取Storyboard文件
     UIStoryboard *launchScreen = [UIStoryboard storyboardWithName:@"LaunchScreen" bundle:nil];
-    // 加载入口的视图控制器
+    // 加载LaunchScreen的初始化视图控制器
     UIViewController *initVC = [launchScreen instantiateInitialViewController];
-    // 获取根视图
-    UIView *rootView = initVC.view;
+    // 获取根视图 (LaunchImage所在的视图)
+    UIView *launchImageView = initVC.view;
     
-    [self.view addSubview:rootView];
+    [self.view addSubview:launchImageView];
     
     // 放大的动画效果
-    [UIView animateWithDuration:2.0 animations:^{
-        rootView.transform = CGAffineTransformMakeScale(3.0, 3.0);
-        rootView.alpha = 0;
+    [UIView animateWithDuration:1.0 animations:^{
+        launchImageView.transform = CGAffineTransformMakeScale(3.0, 3.0);
+        launchImageView.alpha = 0;
     } completion:^(BOOL finished) {
-        [rootView removeFromSuperview];
+        [launchImageView removeFromSuperview];
     }];
 }
 
